2098d6deebb6039c20c65fc4266f342cddf2e646
DHANUSHKUMAR <74861231+dhanushkumar5@users.noreply.github.com>
2023-07-18 00:22:52 +0530

    Add files via upload

---
+++ portfolio/assets/js/script.js
@@ -0,0 +1,6 @@
+function bar(){
+    document.getElementById("menubar").classList.toggle("change");
+    document.getElementById("nav1").classList.toggle("change");
+    document.getElementById("cir").classList.toggle("change-bg");
+
+}
