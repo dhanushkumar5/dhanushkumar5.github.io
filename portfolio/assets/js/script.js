dbbde3a970384e040f6a27690ad388e03996810e
DHANUSHKUMAR <74861231+dhanushkumar5@users.noreply.github.com>
2023-07-18 00:22:08 +0530

    Delete portfolio directory

--- portfolio/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('anima');
-        }
-    });
-    });
-observer.observe(document.querySelector('.eduimg'));
-alert('hi');
